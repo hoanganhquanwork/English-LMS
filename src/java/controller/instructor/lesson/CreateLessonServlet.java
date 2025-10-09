72e0b84b7a888b07ba152c9dc0d8509da49e6268
Lenovo <Lenovo@10.33.87.206>
2025-10-09 14:03:52 +0700

    complete lesson video and create question

---
+++ src/java/controller/instructor/lesson/CreateLessonServlet.java
@@ -0,0 +1,84 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/JSP_Servlet/Servlet.java to edit this template
+ */
+package controller.instructor.lesson;
+
+import dal.LessonDAO;
+import dal.ModuleItemDAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import model.entity.Lesson;
+import model.entity.ModuleItem;
+import util.YouTubeApiClient;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class CreateLessonServlet extends HttpServlet {
+
+    private YouTubeApiClient ytClient;
+
+    public void init() {
+
+        String apiKey = "AIzaSyB6ElDUHgL8NL8Lf3DjIJxhwdXlQ2GyZho";
+        ytClient = new YouTubeApiClient(apiKey);
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        try {
+             int courseId = Integer.parseInt(request.getParameter("courseId"));
+            int moduleId = Integer.parseInt(request.getParameter("moduleId"));
+            String title = request.getParameter("title");
+            String youtubeUrl = request.getParameter("youtubeUrl");
+
+            // Extract videoId và lấy duration
+            String videoId = YouTubeApiClient.extractVideoId(youtubeUrl);
+
+            // 2. Gọi YouTube API để lấy duration
+            String isoDuration = ytClient.fetchIsoDuration(videoId);
+            int durationSec = YouTubeApiClient.isoDurationToSeconds(isoDuration);
+
+            // Insert vào ModuleItem
+            ModuleItem item = new ModuleItem();
+            item.setModuleId(moduleId);
+            item.setItemType("lesson");
+            item.setOrderIndex(1); // tạm
+            item.setRequired(true);
+
+            ModuleItemDAO miDao = new ModuleItemDAO();
+            int moduleItemId = miDao.insertModuleItem(item);
+
+            // Insert vào Lesson
+            Lesson lesson = new Lesson();
+            lesson.setModuleItemId(moduleItemId);
+            lesson.setTitle(title);
+            lesson.setContentType("video");
+            lesson.setVideoUrl(YouTubeApiClient.toEmbedUrl(videoId));
+            lesson.setDurationSec(durationSec);
+            lesson.setTextContent(null);
+
+            LessonDAO lDao = new LessonDAO();
+            lDao.insertLesson(lesson);
+
+//            Lesson lessonSaved = lDao.getLessonById(moduleItemId);
+
+             response.sendRedirect("ManageLessonServlet?courseId=" + courseId + "&moduleId=" + moduleId + "&lessonId=" + moduleItemId);
+        } catch (Exception e) {
+            throw new ServletException(e);
+        }
+    }
+}
---
+++ src/java/model/entity/Question.java
@@ -0,0 +1,77 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model.entity;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Question {
+    private int questionId;
+    private int moduleId;
+    private Integer lessonId;  // Có thể null
+    private String questionText;
+    private String questionType; // 'mcq_single', 'mcq_multiple', 'true_false'
+    private String explanation;
+
+    public Question() {}
+
+    public Question(int questionId, int moduleId, Integer lessonId, String questionText, String questionType, String explanation) {
+        this.questionId = questionId;
+        this.moduleId = moduleId;
+        this.lessonId = lessonId;
+        this.questionText = questionText;
+        this.questionType = questionType;
+        this.explanation = explanation;
+    }
+
+    public int getQuestionId() {
+        return questionId;
+    }
+
+    public void setQuestionId(int questionId) {
+        this.questionId = questionId;
+    }
+
+    public int getModuleId() {
+        return moduleId;
+    }
+
+    public void setModuleId(int moduleId) {
+        this.moduleId = moduleId;
+    }
+
+    public Integer getLessonId() {
+        return lessonId;
+    }
+
+    public void setLessonId(Integer lessonId) {
+        this.lessonId = lessonId;
+    }
+
+    public String getQuestionText() {
+        return questionText;
+    }
+
+    public void setQuestionText(String questionText) {
+        this.questionText = questionText;
+    }
+
+    public String getQuestionType() {
+        return questionType;
+    }
+
+    public void setQuestionType(String questionType) {
+        this.questionType = questionType;
+    }
+
+    public String getExplanation() {
+        return explanation;
+    }
+
+    public void setExplanation(String explanation) {
+        this.explanation = explanation;
+    }
+}
